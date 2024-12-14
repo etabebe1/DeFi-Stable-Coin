4d04bf3a338ae60c8391f5d196dcd8e9f8ab482c
etabebe1 <etabebe1@gmail.com>
2024-12-14 03:33:29 -0500

    Updated DeFiStableCoin

--- src/DeFiStabelCoin.sol
+++ src/DeFiStabelCoin.sol
@@ -9,45 +9,52 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * @title DeFiStableCoin
  * @author Jeremiah A.
  * Collateral: Exogenous
- * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Minting (Stability Mechanism): DeFi (Algorithmic)
  * Value (Relative Stability): Anchored (Pegged to USD)
  * Collateral Type: Crypto
- * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
- * DSCEngine smart contract.
+ * This is the contract meant to be owned by DSCEngine.
+ * It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
  */
-contract DecentralizedStableCoin is ERC20Burnable, Ownable {
-    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
-    error DecentralizedStableCoin__BurnAmountExceedsBalance();
-    error DecentralizedStableCoin__NotZeroAddress();
+contract DeFiStableCoin is ERC20Burnable, Ownable {
+    error DeFiStableCoin__AmountMustBeMoreThanZero();
+    error DeFiStableCoin__BurnAmountExceedsBalance();
+    error DeFiStableCoin__NotZeroAddress();
 
     /*
     In future versions of OpenZeppelin contracts package, Ownable must be declared with an address of the contract owner
     as a parameter.
     For example:
-    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266) {}
+    constructor() ERC20("DeFiStableCoin", "DSC") Ownable(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266) {}
     Related code changes can be viewed in this commit:
     */
-    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    constructor() ERC20("DeFiStableCoin", "DSC") Ownable(msg.sender) {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
+
         if (_amount <= 0) {
-            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+            revert DeFiStableCoin__AmountMustBeMoreThanZero();
         }
         if (balance < _amount) {
-            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+            revert DeFiStableCoin__BurnAmountExceedsBalance();
         }
+
         super.burn(_amount);
     }
 
-    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool, uint256) {
         if (_to == address(0)) {
-            revert DecentralizedStableCoin__NotZeroAddress();
+            revert DeFiStableCoin__NotZeroAddress();
         }
         if (_amount <= 0) {
-            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+            revert DeFiStableCoin__AmountMustBeMoreThanZero();
         }
         _mint(_to, _amount);
-        return true;
+
+        return (true, _amount);
     }
 }
