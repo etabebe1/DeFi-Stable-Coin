f6d6147d8e742bc87effccff7fb39e7afab55090
etabebe1 <etabebe1@gmail.com>
2024-12-19 04:05:39 -0500

    HelperConfig.s.sol initial setup

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,4 +3,23 @@ pragma solidity ^0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 
-contract HelperConfig is Script {}
+contract HelperConfig is Script {
+    // 0x694AA1769357215DE4FAC081bf1f309aDC325306 ETH/USD
+    // 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43 BTC/USD
+
+    struct NetworkConfig {
+        address wBTC_USDPriceFeed;
+        address wETH_USDPriceFeed;
+        address wBTC;
+        address wETH;
+        uint256 deployKey;
+    }
+
+    NetworkConfig public activeNetwork;
+    mapping(uint256 chainId => NetworkConfig) chainIdToNetworkConfig;
+
+    /// Private and Internal vew functions ///
+    function _initializeHelperConfig() internal {}
+
+    function _setActiveNetworkConfig() internal {}
+}
